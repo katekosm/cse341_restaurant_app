084bb5596c08c78573ef9b32dddf89355dc8045c
Angel-L-T <lug21003@byui.edu>
2023-05-31 18:51:15 -0600

    Changes to testing

--- __tests__/routes.t.js
+++ __tests__/routes.t.js
@@ -4,7 +4,6 @@ const { MongoClient } = require("mongodb");
 const app = require("../app");
 const supertest = require("supertest");
 const { expect } = require("@jest/globals");
-const { updateUser } = require("../controllers/users");
 const request = supertest(app);
 
 //Users collection
@@ -26,15 +25,35 @@ describe("User collection tests", () => {
     await connection.close();
   });
 
-  test("responds to /users", async () => {
+  test("GET: users/", async () => {
     const res = await request.get("/users");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
   });
 
-  test("Creates a new user", async () => {
-    const users = db.collection("users");
+  test("POST: users/", async () => {  
+    mockUser = {
+      name: "Test",
+      lastName: "User",
+      nickname: "test.user",
+      email: "test.user@email.com",
+      yearsOfWorking: 10,
+      gender: 1,
+      password: "Test1"
+    };
+    const res = (await request.post("/users").send(mockUser));
+    docId = res.body.insertedId;
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
+  });
 
+  test("GET: users/{id}", async () => {
+    const res = await request.get("/users/" + docId);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+  });
+
+  test("PUT: users/{id}", async () => {  
     mockUser = {
       name: "Test",
       lastName: "User",
@@ -43,43 +62,15 @@ describe("User collection tests", () => {
       yearsOfWorking: 10,
       gender: 1,
       userType: 1,
+      password: "Test1"
     };
-
-    const createdUser = await users.insertOne(mockUser);
-    docId = createdUser.insertedId;
+    const res = (await request.put("/users/" + docId).send(mockUser));
+    expect(res.statusCode).toBe(204);
   });
 
-  test("Finds user", async () => {
-    const users = db.collection("users");
-    //console.log(createdUser.insertedId);
-
-    const insertedUser = await users.findOne({ _id: docId });
-    expect(insertedUser).toEqual(mockUser);
-  });
-
-  test("Updates user", async () => {
-    const updateUser = {
-      name: "Test2",
-      lastName: "User2",
-      nickname: "test2.user",
-      email: "test2.user@email.com",
-      yearsOfWorking: 5,
-      gender: 2,
-      userType: 2,
-    };
-    const users = db.collection("users");
-    //console.log(createdUser.insertedId);
-
-    const updatedUser = await users.replaceOne({ _id: docId }, updateUser);
-    expect(updatedUser.acknowledged).toEqual(true);
-    expect(updatedUser.matchedCount).toEqual(1);
-  });
-
-  test("Deletes user", async () => {
-    const users = db.collection("users");
-
-    const deletedUser = await users.deleteOne({ _id: docId }, true);
-    expect(deletedUser.acknowledged).toEqual(true);
+  test("DELETE: users/{id}", async () => {
+    const res = await request.delete("/users/" + docId);
+    expect(res.statusCode).toBe(200);
   });
 });
 
@@ -102,56 +93,44 @@ describe("Customer collection tests", () => {
     await connection.close();
   });
 
-  test("responds to /customers", async () => {
+  test("GET: customers/", async () => {
     const res = await request.get("/customers");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
   });
 
-  test("Creates a new customer", async () => {
-    const customers = db.collection("customers");
-
+  test("POST: customers/", async () => {  
     mockCustomer = {
-      userName: "User Test",
-      googleId: 1234512345,
-      email: "user.test@email.com",
+      _id: 123451234,
+      userName: "test.test",
+      googleId: "123412341234",
+      email: "test@test.com",
     };
-
-    const createdCustomer = await customers.insertOne(mockCustomer);
-    docId = createdCustomer.insertedId;
+    const res = (await request.post("/customers").send(mockCustomer));
+    docId = res.body.upsertedId;
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
+  });
+  
+  test("GET: customers/{id}", async () => {
+    const res = await request.get("/customers/" + docId);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
   });
 
-  test("Finds customer", async () => {
-    const customers = db.collection("customers");
-    //console.log(createdCustomer.insertedId);
-
-    const insertedCustomer = await customers.findOne({ _id: docId });
-    expect(insertedCustomer).toEqual(mockCustomer);
-  });
-
-  test("Updates customer", async () => {
-    const updateCustomer = {
-      userName: "User Test 2",
-      googleId: 1234512344,
-      email: "user.test2@email.com",
+ test("PUT: customers/{id}", async () => {  
+    mockCustomer = {
+      email: "test2@test.com",
+      userName: "test2.test",
     };
-    const customers = db.collection("customers");
-    //console.log(createdCustomer.insertedId);
-
-    const updatedCustomer = await customers.replaceOne(
-      { _id: docId },
-      updateCustomer
-    );
-    expect(updatedCustomer.acknowledged).toEqual(true);
-    expect(updatedCustomer.matchedCount).toEqual(1);
+    const res = (await request.put("/customers/" + docId).send(mockCustomer));
+    expect(res.statusCode).toBe(204);
   });
 
-  test("Deletes customer", async () => {
-    const customers = db.collection("customers");
-
-    const deletedCustomer = await customers.deleteOne({ _id: docId }, true);
-    expect(deletedCustomer.acknowledged).toEqual(true);
-  });
+  test("DELETE: customers/{id}", async () => {
+    const res = await request.delete("/customers/" + docId);
+    expect(res.statusCode).toBe(200);
+  }); 
 });
 
 //Orders collection
@@ -173,64 +152,55 @@ describe("Order collection tests", () => {
     await connection.close();
   });
 
-  test("responds to /orders", async () => {
+  test("GET: orders/", async () => {
     const res = await request.get("/orders");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
   });
 
-  test("Creates a new order", async () => {
-    const orders = db.collection("orders");
-
+  test("POST: orders/", async () => {  
     mockOrder = {
-      customerId: 123451,
-      orderDate: Date.now(),
-      totalPrice: 12123,
-      paymentMethod: "Cash",
-      paymentStatus: "Completed",
-      shippingAddress: "Anywhere 23, Some place, Some city",
-      shippingStatus: "Sent",
-    };
-
-    const createdOrder = await orders.insertOne(mockOrder);
-    docId = createdOrder.insertedId;
-  });
-
-  test("Finds order", async () => {
-    const orders = db.collection("orders");
-    //console.log(createdOrder.insertedId);
-
-    const insertedOrder = await orders.findOne({ _id: docId });
-    expect(insertedOrder).toEqual(mockOrder);
-  });
-
-  test("Updates order", async () => {
-    const updateOrder = {
-      customerId: 123452,
-      orderDate: Date.now(),
-      totalPrice: 12122,
+      customerId: 12345,
+      orderDate: "2023/05/31",
+      totalPrice: 123,
       paymentMethod: "Credit Card",
       paymentStatus: "Waiting for approval",
-      shippingAddress: "Anywhere 33, Some place, Some city",
-      shippingStatus: "Waiting for payment",
+      shippingAddress: "Somewhere 23, Anywhere, Everywhere",
+      shippingStatus: "Waiting for approval"
     };
-    const orders = db.collection("orders");
-    //console.log(createdOrder.insertedId);
-
-    const updatedOrder = await orders.replaceOne({ _id: docId }, updateOrder);
-    expect(updatedOrder.acknowledged).toEqual(true);
-    expect(updatedOrder.matchedCount).toEqual(1);
+    const res = (await request.post("/orders").send(mockOrder));
+    docId = res.body.insertedId;
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
   });
 
-  test("Deletes order", async () => {
-    const orders = db.collection("orders");
+  test("GET: orders/{id}", async () => {
+    const res = await request.get("/orders/" + docId);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
+  });
 
-    const deletedOrder = await orders.deleteOne({ _id: docId }, true);
-    expect(deletedOrder.acknowledged).toEqual(true);
+  test("PUT: orders/{id}", async () => {  
+    mockOrder = {
+      customerId: 12345,
+      orderDate: "2023/06/01",
+      totalPrice: 1234,
+      paymentMethod: "Credit Card",
+      paymentStatus: "Completed",
+      shippingAddress: "Somewhere 23, Anywhere, Everywhere",
+      shippingStatus: "Sent"
+    };
+    const res = (await request.put("/orders/" + docId).send(mockOrder));
+    expect(res.statusCode).toBe(204);
+  });
+
+  test("DELETE: orders/{id}", async () => {
+    const res = await request.delete("/orders/" + docId);
+    expect(res.statusCode).toBe(200);
   });
 });
 
-//Menu collection
+//Menus collection
 describe("Menu collection tests", () => {
   let connection;
   let db;
@@ -249,55 +219,46 @@ describe("Menu collection tests", () => {
     await connection.close();
   });
 
-  test("responds to /menus", async () => {
+  test("GET: menus/", async () => {
     const res = await request.get("/menus");
     expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
     expect(res.statusCode).toBe(200);
   });
 
-  test("Creates a new menu", async () => {
-    const menus = db.collection("menu");
-
+  test("POST: menus/", async () => {  
     mockMenu = {
-      name: "Menu Name",
-      description: "Menu description",
+      name: "Healthy Food",
+      description: "Healthy menu for everybody",
       price: 1234,
       category: "Vegan",
-      availability: 1,
+      availability: 1
     };
-
-    const createdMenu = await menus.insertOne(mockMenu);
-    docId = createdMenu.insertedId;
+    const res = (await request.post("/menus").send(mockMenu));
+    docId = res.body.insertedId;
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(201);
   });
 
-  test("Finds menu", async () => {
-    const menus = db.collection("menu");
-    //console.log(createdMenu.insertedId);
-
-    const insertedMenu = await menus.findOne({ _id: docId });
-    expect(insertedMenu).toEqual(mockMenu);
+  test("GET: menus/{id}", async () => {
+    const res = await request.get("/menus/" + docId);
+    expect(res.header["content-type"]).toBe("application/json; charset=utf-8");
+    expect(res.statusCode).toBe(200);
   });
 
-  test("Updates menu", async () => {
-    const updateMenu = {
-      name: "Menu Name 2",
-      description: "Menu description 2",
-      price: 12345,
-      category: "Vegan 2",
-      availability: 0,
+  test("PUT: menus/{id}", async () => {  
+    mockMenu = {
+      name: "Healthy Food 2",
+      description: "Healthy menu for everybody",
+      price: 1235,
+      category: "Vegan",
+      availability: 0
     };
-    const menus = db.collection("menu");
-    //console.log(createdMenu.insertedId);
-
-    const updatedMenu = await menus.replaceOne({ _id: docId }, updateMenu);
-    expect(updatedMenu.acknowledged).toEqual(true);
-    expect(updatedMenu.matchedCount).toEqual(1);
+    const res = (await request.put("/menus/" + docId).send(mockMenu));
+    expect(res.statusCode).toBe(204);
   });
 
-  test("Deletes menu", async () => {
-    const menus = db.collection("menu");
-
-    const deletedMenu = await menus.deleteOne({ _id: docId }, true);
-    expect(deletedMenu.acknowledged).toEqual(true);
+  test("DELETE: menus/{id}", async () => {
+    const res = await request.delete("/menus/" + docId);
+    expect(res.statusCode).toBe(200);
   });
 });
